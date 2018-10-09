171252690612a7b44b6b0cba708be09e8cdd3be3
Ravi <ravi.tejarockon@gmail.com>
2018-10-09 04:05:41 +0000

    all

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteException;
+import org.apache.ignite.Ignition;
+public class HelloWorld {
+  public static void main(String[] args) throws IgniteException {
+    try (Ignite ignite = Ignition.start("examples/config/example-ignite.xml")) {
+      // Put values in cache.
+      IgniteCache<Integer, String> cache = ignite.getOrCreateCache("myCache");
+      cache.put(1, "Hello");
+      cache.put(2, "World!");
+      // Get values from cache
+      // Broadcast 'Hello World' on all the nodes in the cluster.
+      ignite.compute().broadcast(()->System.out.println(cache.get(1) + " " + cache.get(2)));
+    }
+  }
+}
